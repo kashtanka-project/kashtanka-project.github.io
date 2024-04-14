2592ac6f611593cb84f3396d877a5593268580cb
tentoshka <lnvdmitry@outlook.com>
2024-04-14 22:42:14 +0700

    first commit

---
+++ out/kashtanka/static_website.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 1.10.844 {:static-fns true, :optimize-constants true}
+goog.provide('kashtanka.static_website');
+goog.require('cljs.core');
+goog.require('cljs.core.constants');
+console.log("HI, Github Pages!");
---
+++ src/kashtanka/static_website.cljs
@@ -0,0 +1,3 @@
+(ns kashtanka.static-website)
+
+(js/console.log "HI, Github Pages!")
